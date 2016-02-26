65ab1579e82beabcd84579737c6b57318ccc780c
Alexey Kostin <alexey.kostin@digitalbarriers.com>
2016-02-26 13:52:37 +0000

    Enable pic for x264 library

--- extra/buildFFmpegOnDebian.sh
+++ extra/buildFFmpegOnDebian.sh
@@ -73,7 +73,7 @@ else
     wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
     tar xjvf last_x264.tar.bz2
     cd x264-snapshot*
-    PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+    PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic
     PATH="$HOME/bin:$PATH" make -j 8
     make install
     make distclean
